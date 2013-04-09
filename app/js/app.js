536f2ecda2fd5da81be180c37149b9882ac32af1
qmliu <qmliu@pearlinpalm.com>
2013-04-09 18:37:48 +0800

    update files

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,16 @@
-'use strict';
-
-
-// Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
+angular.module('leyou', []).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/huodong', {templateUrl: 'partials/huodong.html', controller: huodong});
-    $routeProvider.when('/jingping', {templateUrl: 'partials/jingping.html', controller: jingping});
-    $routeProvider.when('/luntan', {templateUrl: 'partials/luntan.html', controller: luntan});
-    $routeProvider.when('/zhanhu', {templateUrl: 'partials/zhanhu.html', controller: zhanhu});
-    $routeProvider.otherwise({redirectTo: '/jingping'});
+    $routeProvider.when('/huodong', {templateUrl: 'partials/huodong.html', controller: huodong}).
+    when('/jingping', {templateUrl: 'partials/jingping.html', controller: jingping}).
+    when('/luntan', {templateUrl: 'partials/luntan.html', controller: luntan}).
+    when('/zhanhu', {templateUrl: 'partials/zhanhu.html', controller: zhanhu}).
+    otherwise({redirectTo: '/jingping'});
   }]);
+
+var menu = [
+{href:"#/huodong", :img_src:"img/hd.png",alt:"活动",color:"#399EFF"}
+{href:"#/jingping",:img_src:"img/jp.png",alt:"精品",color:"#FF3863"}
+{href:"#/luntan",  :img_src:"img/lt.png",alt:"论坛",color:"#7BAA08"}
+{href:"#/zhanhu",  :img_src:"img/zh.png",alt:"账户",color:"#EFA210"}
+
+];

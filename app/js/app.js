68ffcb2cd60514576c6bcd6819fdbfd5f764a199
qmliu <qmliu@pearlinpalm.com>
2013-04-10 02:39:32 +0800

    update files at home

--- app/js/app.js
+++ app/js/app.js
@@ -1,49 +1,52 @@
 angular.module('leyou', ['leyou.service']).
 config(['$routeProvider', function($routeProvider) {
 	$routeProvider.when('/huodong', {
-		templateUrl: 'partials/huodong.html'
-		,controller: huodong
+		templateUrl: 'partials/huodong.html',
+		controller: huodong
 	}).
 	when('/jingping', {
-		templateUrl: 'partials/jingping.html'
-		,controller: jingping
+		templateUrl: 'partials/jingping.html',
+		controller: jingping
 	}).
 	when('/luntan', {
-		templateUrl: 'partials/luntan.html'
-		,controller: luntan
+		templateUrl: 'partials/luntan.html',
+		controller: luntan
 	}).
 	when('/zhanhu', {
-		templateUrl: 'partials/zhanhu.html'
-		,controller: zhanhu
+		templateUrl: 'partials/zhanhu.html',
+		controller: zhanhu
 	}).
 	otherwise({
 		redirectTo: '/jingping'
 	});
 }]);
 
-angular.module('leyou.service',[]).value('common', {
+angular.module('leyou.service', []).value('common', {
 	menu: [{
-		href: "#/huodong"
-		,img_src: "img/hd.png"
-		,alt: "活动"
-		,color: "#399EFF"
-	}, {
-		href: "#/jingping"
-		,img_src: "img/jp.png"
-		,alt: "精品"
-		,color: "#FF3863"
-	}, {
-		href: "#/luntan"
-		,img_src: "img/lt.png"
-		,alt: "论坛"
-		,color: "#7BAA08"
-	}, {
-		href: "#/zhanhu"
-		,img_src: "img/zh.png"
-		,alt: "账户"
-		,color: "#EFA210"
-	}]
-	,logo: "img/logo.png"
+		href: "#/huodong",
+		img_src: "img/hd.png",
+		alt: "活动",
+		color: "#399EFF"
+	},
+	{
+		href: "#/jingping",
+		img_src: "img/jp.png",
+		alt: "精品",
+		color: "#FF3863"
+	},
+	{
+		href: "#/luntan",
+		img_src: "img/lt.png",
+		alt: "论坛",
+		color: "#7BAA08"
+	},
+	{
+		href: "#/zhanhu",
+		img_src: "img/zh.png",
+		alt: "账户",
+		color: "#EFA210"
+	}],
+	logo: "img/logo.png"
 }).factory("notify", ['$window', function(win) {
 	var msgs = [];
 	return function(msg) {
@@ -55,5 +58,4 @@ angular.module('leyou.service',[]).value('common', {
 	};
 
 }]);
-
 

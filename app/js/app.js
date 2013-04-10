d21cb6b4f361a9a261cfc9ce1b798460757d4701
qmliu <qmliu@pearlinpalm.com>
2013-04-10 18:53:57 +0800

    add pages and data

--- app/js/app.js
+++ app/js/app.js
@@ -4,6 +4,14 @@ config(['$routeProvider', function($routeProvider) {
 		templateUrl: 'partials/huodong.html',
 		controller: huodong
 	}).
+	when('/huodong_zs', {
+		templateUrl: 'partials/huodong_zs.html',
+		controller: huodongZs
+	}).
+	when('/huodong_jl', {
+		templateUrl: 'partials/huodong_jl.html',
+		controller: huodongJl
+	}).
 	when('/jingping', {
 		templateUrl: 'partials/jingping.html',
 		controller: jingping
@@ -46,7 +54,36 @@ angular.module('leyou.service', []).value('common', {
 		alt: "账户",
 		color: "#EFA210"
 	}],
-	logo: "img/logo.png"
+	logo: "img/logo.png",
+  interface_url:"http://192.168.0.252:7200/",
+  games:[
+    {id:14 , short_name_en:"sy", name_cn:"圣域之战",src:"img/img_jp_01.png",alt:""},
+    {id:6  , short_name_en:"sg", name_cn:"明珠三国",src:"img/img_jp_02.png",alt:""},
+    {id:13 , short_name_en:"ss", name_cn:"杀神"    ,src:"img/img_jp_03.png",alt:""},
+    {id:10 , short_name_en:"xy", name_cn:"明珠轩辕",src:"img/img_jp_04.png",alt:""},
+    {id:8  , short_name_en:"hx", name_cn:"明珠幻想",src:"img/img_jp_05.png",alt:""},
+    {id:9  , short_name_en:"xyj",name_cn:"明珠西游",src:"img/img_jp_06.png",alt:""},
+    {id:2  , short_name_en:"wl", name_cn:"武林"    ,src:"img/img_jp_07.png",alt:""}
+    ],
+    zhanhu_icons:[
+    {name: "游戏充值",img_src:"img/img_zh_01.png",link:"#"},
+    {name: "余额查询",img_src:"img/img_zh_02.png",link:"#"},
+    {name:"设置密保",img_src:"img/img_zh_03.png",link:"#"},
+    {name:"找回密码",img_src:"img/img_zh_04.png",link:"#"}
+    ],
+  libaos:[
+    {id:1,game_id:15,game_name:"圣域之战",name:"特权礼包",desc:"一盒酥*10",points:100},
+    {id:2,game_id: 6,game_name:"明珠三国",name:"特权礼包",desc:"一盒酥*10",points:100},
+    {id:3,game_id:12,game_name:"杀神"    ,name:"特权礼包",desc:"一盒酥*10",points:100},
+    {id:4,game_id:11,game_name:"明珠轩辕",name:"特权礼包",desc:"一盒酥*10",points:100},
+    {id:5,game_id:10,game_name:"明珠幻想",name:"特权礼包",desc:"一盒酥*10",points:100},
+    {id:6,game_id:15,game_name:"明珠西游",name:"特权礼包",desc:"一盒酥*10",points:100},
+    {id:6,game_id:12,game_name:"武林"    ,name:"特权礼包",desc:"一盒酥*10",points:100},
+    {id:6,game_id:9 ,game_name:"明珠西游",name:"特权礼包",desc:"一盒酥*10",points:100},
+    {id:6,game_id:8 ,game_name:"武林"    ,name:"特权礼包",desc:"一盒酥*10",points:100}
+    ],
+
+
 }).factory("notify", ['$window', function(win) {
 	var msgs = [];
 	return function(msg) {
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,16 +2,49 @@
 var jingping = ["$scope", "common", function($scope, common) {
 	$scope.title = "乐游互动中心";
 	$scope.menu = common.menu;
+  var games = common.games;
+ if(games && games.length > 0){
+  $scope.top_game = games[0];
+  $scope.left_games = [];
+  $scope.right_games = [];
+
+  for(var i=1;i<games.length;i++){
+    if(i%2 == 0){
+      $scope.left_games.push(games[i]);
+    }else{
+      $scope.right_games.push(games[i]);
+    }
+  }
+ }
 }];
 
-var huodong = ["$scope", "notify", function($scope, notify) {
+var huodong = ["$scope", "common","$http", function($scope, common,$http) {
 	$scope.title = "活动";
-	$scope.msg = notify;
+  $scope.libaos = common.libaos;
 }];
-var luntan = ["$scope", function($scope) {
+var huodongZs = ["$scope", "common","$http", function($scope, common,$http) {
+	$scope.title = "活动助手";
+  $http.jsonp(common.interface_url+"leyou/huodong_zs.json?callback=JSON_CALLBACK").success(function(res,status){
+    $scope.huodongs = res;
+  });
+}];
+var huodongJl = ["$scope", "common",'$http', function($scope, common,$http) {
+	$scope.title = "兑换记录";
+  $http.jsonp(common.interface_url+"leyou/huodong_jl.json?callback=JSON_CALLBACK&user_id=183746").success(function(res,status){
+    $scope.histories = res;
+  });
+}];
+var luntan = ["$scope","common","$http", function($scope,common,$http) {
 	$scope.title = "论坛";
+	$scope.menu = common.menu;
+	$scope.games = common.games;
+  $http.jsonp(common.interface_url+"leyou/news/bulletins.mob?callback=JSON_CALLBACK&cat_id=1").success(function(res,status){
+    $scope.bulletins = res;
+  });
 }];
-var zhanhu = ["$scope", function($scope) {
+var zhanhu = ["$scope","common", function($scope,common) {
 	$scope.title = "账户";
+	$scope.menu = common.menu;
+	$scope.icons = common.zhanhu_icons;
 }];
 

0f3fa1a15f6e76c0ca5ac839f8978fbed15d361a
qmliu <qmliu@pearlinpalm.com>
2013-04-23 19:21:23 +0800

    add download function

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,7 @@
-angular.module('leyou', ['leyou.service']).
+angular.module('leyou', ['leyou.service', 'ui.bootstrap']).
 config(['$routeProvider', function($routeProvider) {
 	$routeProvider.when('/huodong', {
-    templateUrl: 'partials/huodong.html',
+		templateUrl: 'partials/huodong.html',
 		controller: huodong
 	}).
 	when('/huodong_zs', {
@@ -63,36 +63,294 @@ angular.module('leyou.service', []).value('common', {
 		color: "#EFA210"
 	}],
 	logo: "img/logo.png",
-  interface_url:"http://192.168.0.252:7200/",
-  icon_root:"http://m.pipgame.cn/game_logos/",
-  games:[
-    {id:"14" , short_name_en:"sy", name_cn:"龙斗士"  ,src:"img/img_jp_01.png",alt:"",version:"1.0",updated_at:"2013-04-01",style:"策略战争",size:"7.8M",screen:"480x800",description:"是一款多人在线的网络策略战争类游戏。 游戏将玩家带回西方中世纪一个充满魔幻，梦想的战乱年代。玩家将要扮演一位拥有理想与信念的君主，占据一座城堡要塞募集来自世界大陆四方的英雄强者，在战火纷乱的年代中保护自已的子民，并建立稳定而强大的政权，最终统一大陆，成为一代霸者。",package_name:"com.pip.mzcity",cycles:[ {id:"1",src:"http://m.pipgame.com/cycle_imgs/20130402100729514.jpg" }, {id:"1",src:"http://m.pipgame.com/cycle_imgs/201304021006584268.jpg"}, {id:"1",src:"http://m.pipgame.com/cycle_imgs/201304021008123788.jpg"}]},
-    {id:"6"  , short_name_en:"sg", name_cn:"明珠三国",src:"img/img_jp_02.png",alt:"",version:"1.0",updated_at:"2013-04-01",style:"策略战争",size:"7.8M",screen:"480x800",package_name:"com.pip.CCAAGB05"},
-    {id:"13" , short_name_en:"ss", name_cn:"杀神"    ,src:"img/img_jp_03.png",alt:"",version:"1.0",updated_at:"2013-04-01",style:"策略战争",size:"7.8M",screen:"480x800",package_name:"com.pip.ss"},
-    {id:"10" , short_name_en:"xy", name_cn:"明珠轩辕",src:"img/img_jp_04.png",alt:"",version:"1.0",updated_at:"2013-04-01",style:"策略战争",size:"7.8M",screen:"480x800",package_name:"shaft.android"},
-    {id:"8"  , short_name_en:"hx", name_cn:"明珠幻想",src:"img/img_jp_05.png",alt:"",version:"1.0",updated_at:"2013-04-01",style:"策略战争",size:"7.8M",screen:"480x800",package_name:"com.pip.itimes"},
-    {id:"9"  , short_name_en:"xyj",name_cn:"明珠西游",src:"img/img_jp_06.png",alt:"",version:"1.0",updated_at:"2013-04-01",style:"策略战争",size:"7.8M",screen:"480x800",package_name:"com.pip.android.xiyou"},
-    {id:"2"  , short_name_en:"wl", name_cn:"武林"    ,src:"img/img_jp_07.png",alt:"",version:"1.0",updated_at:"2013-04-01",style:"策略战争",size:"7.8M",screen:"480x800",package_name:"com.pip.wulin2"}
-    ],
-    zhanhu_icons:[
-    {name: "游戏充值",img_src:"img/img_zh_01.png",link:"#"},
-    {name: "余额查询",img_src:"img/img_zh_02.png",link:"#"},
-    {name:"设置密保",img_src:"img/img_zh_03.png",link:"#"},
-    {name:"找回密码",img_src:"img/img_zh_04.png",link:"#"},
-    {name:"找回密码",img_src:"img/img_zh_04.png",link:"#/upload"}
-    ],
-  libaos:[
-    {id:1,game_id:15,game_name:"圣域之战",name:"特权礼包",desc:"一盒酥*10",points:100},
-    {id:2,game_id: 6,game_name:"明珠三国",name:"特权礼包",desc:"一盒酥*10",points:100},
-    {id:3,game_id:12,game_name:"杀神"    ,name:"特权礼包",desc:"一盒酥*10",points:100},
-    {id:4,game_id:11,game_name:"明珠轩辕",name:"特权礼包",desc:"一盒酥*10",points:100},
-    {id:5,game_id:10,game_name:"明珠幻想",name:"特权礼包",desc:"一盒酥*10",points:100},
-    {id:6,game_id:15,game_name:"明珠西游",name:"特权礼包",desc:"一盒酥*10",points:100},
-    {id:6,game_id:12,game_name:"武林"    ,name:"特权礼包",desc:"一盒酥*10",points:100},
-    {id:6,game_id:9 ,game_name:"明珠西游",name:"特权礼包",desc:"一盒酥*10",points:100},
-    {id:6,game_id:8 ,game_name:"武林"    ,name:"特权礼包",desc:"一盒酥*10",points:100}
-    ],
-
+	interface_url: "http://192.168.0.252:7200/",
+	icon_root: "http://m.pipgame.cn/game_logos/",
+	games: [{
+		id: "14",
+		short_name_en: "sy",
+		name_cn: "龙斗士",
+		src: "img/img_jp_01.png",
+		alt: "",
+		version: "1.0",
+		updated_at: "2013-04-01",
+		style: "策略战争",
+		size: "7.8M",
+		screen: "480x800",
+		package_name: "com.pip.mzcity",
+		description: "是一款多人在线的网络策略战争类游戏。 游戏将玩家带回西方中世纪一个充满魔幻，梦想的战乱年代。玩家将要扮演一位拥有理想与信念的君主，占据一座城堡要塞募集来自世界大陆四方的英雄强者，在战火纷乱的年代中保护自已的子民，并建立稳定而强大的政权，最终统一大陆，成为一代霸者。",
+    download_url: "http://3g.pipgame.com/c?gn=sy&ft=ShengYu_Android_advanced.apk",
+		cycles: [{
+			id: "1",
+			src: "http://m.pipgame.com/cycle_imgs/20130402100729514.jpg"
+		},
+		{
+			id: "1",
+			src: "http://m.pipgame.com/cycle_imgs/201304021006584268.jpg"
+		},
+		{
+			id: "1",
+			src: "http://m.pipgame.com/cycle_imgs/201304021008123788.jpg"
+		}]
+	},
+	{
+		id: "6",
+		short_name_en: "sg",
+		name_cn: "明珠三国",
+		src: "img/img_jp_02.png",
+		alt: "",
+		version: "1.0",
+		updated_at: "2013-04-01",
+		style: "策略战争",
+		size: "7.8M",
+		screen: "480x800",
+		package_name: "com.pip.CCAAGB05",
+		description: "是一款多人在线的网络策略战争类游戏。 游戏将玩家带回西方中世纪一个充满魔幻，梦想的战乱年代。玩家将要扮演一位拥有理想与信念的君主，占据一座城堡要塞募集来自世界大陆四方的英雄强者，在战火纷乱的年代中保护自已的子民，并建立稳定而强大的政权，最终统一大陆，成为一代霸者。",
+    download_url: "http://3g.pipgame.com/c?gn=sg&ft=Sanguo_AndroidAuto_New_Large_CGWPIP99.apk",
+		cycles: [{
+			id: "1",
+			src: "http://m.pipgame.com/cycle_imgs/20130402100729514.jpg"
+		},
+		{
+			id: "1",
+			src: "http://m.pipgame.com/cycle_imgs/201304021006584268.jpg"
+		},
+		{
+			id: "1",
+			src: "http://m.pipgame.com/cycle_imgs/201304021008123788.jpg"
+		}]
+	},
+	{
+		id: "13",
+		short_name_en: "ss",
+		name_cn: "杀神",
+		src: "img/img_jp_03.png",
+		alt: "",
+		version: "1.0",
+		updated_at: "2013-04-01",
+		style: "策略战争",
+		size: "7.8M",
+		screen: "480x800",
+		package_name: "com.pip.android.ss",
+		description: "是一款多人在线的网络策略战争类游戏。 游戏将玩家带回西方中世纪一个充满魔幻，梦想的战乱年代。玩家将要扮演一位拥有理想与信念的君主，占据一座城堡要塞募集来自世界大陆四方的英雄强者，在战火纷乱的年代中保护自已的子民，并建立稳定而强大的政权，最终统一大陆，成为一代霸者。",
+    download_url: "http://3g.pipgame.com/c?gn=ss&ft=SS_Android_CGWPIP99.apk",
+		cycles: [{
+			id: "1",
+			src: "http://m.pipgame.com/cycle_imgs/20130402100729514.jpg"
+		},
+		{
+			id: "1",
+			src: "http://m.pipgame.com/cycle_imgs/201304021006584268.jpg"
+		},
+		{
+			id: "1",
+			src: "http://m.pipgame.com/cycle_imgs/201304021008123788.jpg"
+		}]
+	},
+	{
+		id: "10",
+		short_name_en: "xy",
+		name_cn: "明珠轩辕",
+		src: "img/img_jp_04.png",
+		alt: "",
+		version: "1.0",
+		updated_at: "2013-04-01",
+		style: "策略战争",
+		size: "7.8M",
+		screen: "480x800",
+		package_name: "shaft.android",
+		description: "是一款多人在线的网络策略战争类游戏。 游戏将玩家带回西方中世纪一个充满魔幻，梦想的战乱年代。玩家将要扮演一位拥有理想与信念的君主，占据一座城堡要塞募集来自世界大陆四方的英雄强者，在战火纷乱的年代中保护自已的子民，并建立稳定而强大的政权，最终统一大陆，成为一代霸者。",
+    download_url: "http://3g.pipgame.com/c?gn=xy&ft=Shaft_Android_CGWPIP99.apk",
+		cycles: [{
+			id: "1",
+			src: "http://m.pipgame.com/cycle_imgs/20130402100729514.jpg"
+		},
+		{
+			id: "1",
+			src: "http://m.pipgame.com/cycle_imgs/201304021006584268.jpg"
+		},
+		{
+			id: "1",
+			src: "http://m.pipgame.com/cycle_imgs/201304021008123788.jpg"
+		}]
+	},
+	{
+		id: "8",
+		short_name_en: "hx",
+		name_cn: "明珠幻想",
+		src: "img/img_jp_05.png",
+		alt: "",
+		version: "1.0",
+		updated_at: "2013-04-01",
+		style: "策略战争",
+		size: "7.8M",
+		screen: "480x800",
+		package_name: "com.pip.itimes",
+		description: "是一款多人在线的网络策略战争类游戏。 游戏将玩家带回西方中世纪一个充满魔幻，梦想的战乱年代。玩家将要扮演一位拥有理想与信念的君主，占据一座城堡要塞募集来自世界大陆四方的英雄强者，在战火纷乱的年代中保护自已的子民，并建立稳定而强大的政权，最终统一大陆，成为一代霸者。",
+    download_url:"http://3g.pipgame.com/c?gn=hx&ft=FiTAndroidAuto_CGWPIP99.apk",
+		cycles: [{
+			id: "1",
+			src: "http://m.pipgame.com/cycle_imgs/20130402100729514.jpg"
+		},
+		{
+			id: "1",
+			src: "http://m.pipgame.com/cycle_imgs/201304021006584268.jpg"
+		},
+		{
+			id: "1",
+			src: "http://m.pipgame.com/cycle_imgs/201304021008123788.jpg"
+		}]
+	},
+	{
+		id: "9",
+		short_name_en: "xyj",
+		name_cn: "明珠西游",
+		src: "img/img_jp_06.png",
+		alt: "",
+		version: "5.0",
+		updated_at: "2013-04-01",
+		style: "策略战争",
+		size: "7.8M",
+		screen: "480x800",
+		package_name: "com.pip.android.xiyou",
+		description: "是一款多人在线的网络策略战争类游戏。 游戏将玩家带回西方中世纪一个充满魔幻，梦想的战乱年代。玩家将要扮演一位拥有理想与信念的君主，占据一座城堡要塞募集来自世界大陆四方的英雄强者，在战火纷乱的年代中保护自已的子民，并建立稳定而强大的政权，最终统一大陆，成为一代霸者。",
+    download_url: "http://3g.pipgame.com/c?gn=xyj&ft=XiyouAndroid_CGWPIP99.apk",
+		cycles: [{
+			id: "1",
+			src: "http://m.pipgame.com/cycle_imgs/20130402100729514.jpg"
+		},
+		{
+			id: "1",
+			src: "http://m.pipgame.com/cycle_imgs/201304021006584268.jpg"
+		},
+		{
+			id: "1",
+			src: "http://m.pipgame.com/cycle_imgs/201304021008123788.jpg"
+		}]
+	},
+	{
+		id: "2",
+		short_name_en: "wl",
+		name_cn: "武林",
+		src: "img/img_jp_07.png",
+		alt: "",
+		version: "1.0",
+		updated_at: "2013-04-01",
+		style: "策略战争",
+		size: "7.8M",
+		screen: "480x800",
+		package_name: "com.pip.wulin2",
+		description: "是一款多人在线的网络策略战争类游戏。 游戏将玩家带回西方中世纪一个充满魔幻，梦想的战乱年代。玩家将要扮演一位拥有理想与信念的君主，占据一座城堡要塞募集来自世界大陆四方的英雄强者，在战火纷乱的年代中保护自已的子民，并建立稳定而强大的政权，最终统一大陆，成为一代霸者。",
+    download_url: "http://3g.pipgame.com/c?gn=wl&ft=Wulin2_AndroidGeneral_CGWPIP99.apk",
+		cycles: [{
+			id: "1",
+			src: "http://m.pipgame.com/cycle_imgs/20130402100729514.jpg"
+		},
+		{
+			id: "1",
+			src: "http://m.pipgame.com/cycle_imgs/201304021006584268.jpg"
+		},
+		{
+			id: "1",
+			src: "http://m.pipgame.com/cycle_imgs/201304021008123788.jpg"
+		}]
+	}],
+	zhanhu_icons: [{
+		name: "游戏充值",
+		img_src: "img/img_zh_01.png",
+		link: "#"
+	},
+	{
+		name: "余额查询",
+		img_src: "img/img_zh_02.png",
+		link: "#"
+	},
+	{
+		name: "设置密保",
+		img_src: "img/img_zh_03.png",
+		link: "#"
+	},
+	{
+		name: "找回密码",
+		img_src: "img/img_zh_04.png",
+		link: "#"
+	},
+	{
+		name: "找回密码",
+		img_src: "img/img_zh_04.png",
+		link: "#/upload"
+	}],
+	libaos: [{
+		id: 1,
+		game_id: 15,
+		game_name: "圣域之战",
+		name: "特权礼包",
+		desc: "一盒酥*10",
+		points: 100
+	},
+	{
+		id: 2,
+		game_id: 6,
+		game_name: "明珠三国",
+		name: "特权礼包",
+		desc: "一盒酥*10",
+		points: 100
+	},
+	{
+		id: 3,
+		game_id: 12,
+		game_name: "杀神",
+		name: "特权礼包",
+		desc: "一盒酥*10",
+		points: 100
+	},
+	{
+		id: 4,
+		game_id: 11,
+		game_name: "明珠轩辕",
+		name: "特权礼包",
+		desc: "一盒酥*10",
+		points: 100
+	},
+	{
+		id: 5,
+		game_id: 10,
+		game_name: "明珠幻想",
+		name: "特权礼包",
+		desc: "一盒酥*10",
+		points: 100
+	},
+	{
+		id: 6,
+		game_id: 15,
+		game_name: "明珠西游",
+		name: "特权礼包",
+		desc: "一盒酥*10",
+		points: 100
+	},
+	{
+		id: 6,
+		game_id: 12,
+		game_name: "武林",
+		name: "特权礼包",
+		desc: "一盒酥*10",
+		points: 100
+	},
+	{
+		id: 6,
+		game_id: 9,
+		game_name: "明珠西游",
+		name: "特权礼包",
+		desc: "一盒酥*10",
+		points: 100
+	},
+	{
+		id: 6,
+		game_id: 8,
+		game_name: "武林",
+		name: "特权礼包",
+		desc: "一盒酥*10",
+		points: 100
+	}],
 
 }).factory("notify", ['$window', function(win) {
 	var msgs = [];
@@ -104,17 +362,51 @@ angular.module('leyou.service', []).value('common', {
 		}
 	};
 
-}]).factory("plugins",['$window',function($window){
-    var actions = {};
-    actions.detect_package = function(success,fail,package_name){
-      var fail = fail || function(res){alert(res)}
-      console.log(package_name);
-      cordova.exec(success, fail, "Tools", "detect_package", package_name);
-    }
-    return actions;
+}]).factory("plugins", ['$window', function($window) {
+	var actions = {};
+	actions.detect_package = function(package_name,success, fail) {
+		var fail = fail || function(res) {
+			alert(res)
+		}
+		console.log(package_name);
+		cordova.exec(success, fail, "Tools", "detect_package", package_name);
+	}
+  actions.open_game = function(package_name,success,fail){
+		var fail = fail || function(res) {
+			alert(res)
+		}
+		console.log("open"+package_name);
+		cordova.exec(success, fail, "Tools", "open_game", [package_name]);
 
   }
+  //actions.download = function(url,package_name,$scope){
+  //  var fileTransfer = new FileTransfer();
+  //  var uri = encodeURI(url);
+  //  var filePath = "/mnt/sdcard/download/"+package_name+".apk";
+  //  //$scope.start_download = true;
+  //  fileTransfer.onprogress = function(progressEvent) {
+  //    if (progressEvent.lengthComputable) {
+  //      $scope.complete = parseInt(progressEvent.loaded*100/progressEvent.total);
+  //    } else {
+  //      alert("不支持状态显示");
+  //      //loadingStatus.increment();
+  //    }
+  //  };
 
+  //  fileTransfer.download( uri, filePath,
+  //      function(entry) {
+  //        cordova.exec(null, null, "Tools", "install_package", [entry.fullPath]);
+  //        $scope.start_download = false;
+  //      },
+  //      function(error) {
+  //        alert("下载失败，请重新再试!" + error.code);
+  //      },
+  //      false, { }
+  //      );
+  //}
+	return actions;
+
+}
 
 ]);
 
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,181 +2,282 @@
 var jingping = ["$scope", "common", function($scope, common) {
 	$scope.title = "乐游互动中心";
 	$scope.menu = common.menu;
-  var games = common.games;
-  //$scope.detail = function(game_name,package_name,game_id){
-    //PipGameBoxNew.v_openDetail(game_name,package_name,game_id);
-  //}
- if(games && games.length > 0){
-  $scope.top_game = games[0];
-  $scope.left_games = [];
-  $scope.right_games = [];
+	var games = common.games;
+	//$scope.detail = function(game_name,package_name,game_id){
+	//PipGameBoxNew.v_openDetail(game_name,package_name,game_id);
+	//}
+	if (games && games.length > 0) {
+		$scope.top_game = games[0];
+		$scope.left_games = [];
+		$scope.right_games = [];
 
-
-  for(var i=1,len=games.length;i<len;i++){
-    if(i%2 == 0){
-      $scope.left_games.push(games[i]);
-    }else{
-      $scope.right_games.push(games[i]);
-    }
-  }
- }
+		for (var i = 1, len = games.length; i < len; i++) {
+			if (i % 2 == 0) {
+				$scope.left_games.push(games[i]);
+			} else {
+				$scope.right_games.push(games[i]);
+			}
+		}
+	}
 }];
 
-var huodong = ["$scope", "common","$http", function($scope, common,$http) {
+var huodong = ["$scope", "common", "$http", function($scope, common, $http) {
 	$scope.title = "活动";
-  $scope.libaos = common.libaos;
+	$scope.libaos = common.libaos;
 }];
-var huodongZs = ["$scope", "common","$http", function($scope, common,$http) {
+var huodongZs = ["$scope", "common", "$http", function($scope, common, $http) {
 	$scope.title = "活动助手";
-  $http.jsonp(common.interface_url+"leyou/huodong_zs.json?callback=JSON_CALLBACK").success(function(res,status){
-    $scope.huodongs = res;
-  });
+	$http.jsonp(common.interface_url + "leyou/huodong_zs.json?callback=JSON_CALLBACK").success(function(res, status) {
+		$scope.huodongs = res;
+	});
 }];
-var huodongJl = ["$scope", "common",'$http', function($scope, common,$http) {
+var huodongJl = ["$scope", "common", '$http', function($scope, common, $http) {
 	$scope.title = "兑换记录";
-  $http.jsonp(common.interface_url+"leyou/huodong_jl.json?callback=JSON_CALLBACK&user_id=183746").success(function(res,status){
-    $scope.histories = res;
-  });
+	$http.jsonp(common.interface_url + "leyou/huodong_jl.json?callback=JSON_CALLBACK&user_id=183746").success(function(res, status) {
+		$scope.histories = res;
+	});
 }];
-var luntan = ["$scope","common","$http", function($scope,common,$http) {
+var luntan = ["$scope", "common", "$http", function($scope, common, $http) {
 	$scope.title = "论坛";
 	$scope.menu = common.menu;
 	$scope.games = common.games;
-  $scope.icon_url = common.icon_root;
-  $http.jsonp(common.interface_url+"leyou/news/bulletins.mob?callback=JSON_CALLBACK&cat_id=1").success(function(res,status){
-    $scope.bulletins = res;
-  });
+	$scope.icon_url = common.icon_root;
+	$http.jsonp(common.interface_url + "leyou/news/bulletins.mob?callback=JSON_CALLBACK&cat_id=1").success(function(res, status) {
+		$scope.bulletins = res;
+	});
 }];
-var zhanhu = ["$scope","common", function($scope,common) {
+var zhanhu = ["$scope", "common", function($scope, common) {
 	$scope.title = "账户";
 	$scope.menu = common.menu;
 	$scope.icons = common.zhanhu_icons;
 }];
 //游戏详情控制器
-var games = ["$scope","common","$routeParams","$filter","plugins", function($scope,common,params,$filter,plugins) {
+var games = ["$scope", "common", "$routeParams", "$filter", "plugins", function($scope, common, params, $filter, plugins) {
 	$scope.title = "游戏详情";
-  game = $filter('filter')(common.games,{id:params.id},true)[0];
-  document.addEventListener("deviceready",on_device_ready,false);
-  console.log(game.package_name);
-  function on_device_ready(){
-      plugins.detect_package(function(res){
-        if(res[game.package_name].installed == "1"){
-          $scope.install_version = "已安装版本"+res[game.package_name].version;
+	$scope.display_text = "免费下载";
+	game = $filter('filter')(common.games, {
+		id: params.id
+	},
+	true)[0];
+  game.not_first_download = false;
+	document.addEventListener("deviceready", on_device_ready, false);
+	console.log(game.package_name);
+	function on_device_ready() {
+		var detect = function() {
+      console.log("检测程序")
+			plugins.detect_package([game.package_name], function(res) {
+				if (res[game.package_name].installed == "1") {
+					$scope.display_text = "开始游戏";
+					//game.game_state: 0未安装,1已安装，2可更新
+					game.game_state = "1";
+					if (res[game.package_name].version < game.version) {
+						$scope.display_text = "更新游戏";
+						game.game_state = "2";
+					}
+				}else{
+					$scope.display_text = "免费下载";
         }
-      },null,[game.package_name]);
+			},
+			null);
+
+		}
+    detect();
+    var on_resume = function(){
+      detect();
+      $scope.$apply();
     }
-  $scope.icon_url = common.icon_root+"iphone"+params.id+".png";
-  $scope.game = game;
+		document.addEventListener("resume", on_resume, false);
+
+	}
+
+	$scope.open_game = function(ev) {
+		switch (game.game_state) {
+		case "1":
+			plugins.open_game(game.package_name, function() {
+				console.log("i open the game" + game.short_name_en)
+			})
+			console.log("open game");
+			break;
+		case "2":
+			console.log("update game");
+			if (confirm("游戏可更新，要更新游戏么？")) {
+				console.log("更新游戏");
+				download();
+			} else {
+				plugins.open_game(game.package_name, function(res) {
+					console.log("i open the game" + game.short_name_en)
+				},
+				null);
+				console.log("打开游戏");
+			}
+      break;
+    default:
+      console.log(!game.not_first_download);
+      if(!game.not_first_download){
+        $scope.display_text = "下载中...";
+        game.not_first_download = true;
+        download();
+      }
+			break;
+		}
+
+	}
+
+	//$scope.start_download = true;
+	var download = function() {
+		var fileTransfer = new FileTransfer();
+		var uri = encodeURI(game.download_url);
+		var filePath = "/mnt/sdcard/download/" + game.package_name + ".apk";
+		$scope.start_download = true;
+		fileTransfer.onprogress = function(progressEvent) {
+			if (progressEvent.lengthComputable) {
+				$scope.complete = parseInt(progressEvent.loaded * 100 / progressEvent.total);
+				$scope.$apply();
+			} else {
+				alert("不支持状态显示");
+				//loadingStatus.increment();
+			}
+		};
+
+		fileTransfer.download(uri, filePath, function(entry) {
+			cordova.exec(function(res) {
+				//game.game_state="1";
+				console.log("----------------------install ok");
+			},
+			null, "Tools", "install_package", [entry.fullPath]);
+			$scope.start_download = false;
+		},
+		function(error) {
+			alert("下载失败，请重新再试!" + error.code);
+		},
+		false, {});
+	}
+	$scope.icon_url = common.icon_root + "iphone" + params.id + ".png";
+	$scope.game = game;
 	$scope.menu = common.menu;
 	$scope.icons = common.zhanhu_icons;
 }];
-var download_file = ["$scope","common", function($scope,common) {
+var download_file = ["$scope", "common", function($scope, common) {
 	$scope.title = "下载文件";
 	$scope.menu = common.menu;
 	$scope.icons = common.zhanhu_icons;
 }];
 
-var upload_file = ["$scope","common","$timeout", function($scope,common,$timeout) {
+var upload_file = ["$scope", "common", "$timeout", function($scope, common, $timeout) {
 	$scope.title = "上传文件";
-  $scope.detail = function(package_name){
-    alert("test");
-    PipGameBoxNew.v_openGame(package_name);
-    return false;
-  }
+	$scope.detail = function(package_name) {
+		alert("test");
+		PipGameBoxNew.v_openGame(package_name);
+		return false;
+	}
 
-  //test timeout data-binding"
-  var stop;
-  var add = function(){
-    stop=$timeout(function(){
-      $scope.title += "0";
-      console.log($scope.title);
-      add();
+	//test timeout data-binding"
+	var stop;
+	var add = function() {
+		stop = $timeout(function() {
+			$scope.title += "0";
+			console.log($scope.title);
+			add();
 
-    },1000);
-  }
-  //add();
+		},
+		1000);
+	}
+	//add();
 	$scope.menu = common.menu;
 	$scope.icons = common.zhanhu_icons;
-  document.addEventListener("deviceready",onDeviceReady,false);
-  var pictureSource,destinationType;
-  function onDeviceReady() {
-    pictureSource=navigator.camera.PictureSourceType;
-    destinationType=navigator.camera.DestinationType;
-    $scope.device_id = device.uuid
-    $scope.cordova = device.cordova
-    $scope.name = device.name
-    $scope.platform = device.platform
-    $scope.version = device.version
-    $scope.type =  navigator.network.connection.type
-  }
+	document.addEventListener("deviceready", onDeviceReady, false);
+	var pictureSource, destinationType;
+	function onDeviceReady() {
+		pictureSource = navigator.camera.PictureSourceType;
+		destinationType = navigator.camera.DestinationType;
+		$scope.device_id = device.uuid
+		$scope.cordova = device.cordova
+		$scope.name = device.name
+		$scope.platform = device.platform
+		$scope.version = device.version
+		$scope.type = navigator.network.connection.type
+	}
 
-  function onPhotoDataSuccess(){
-    alert("ok");
-  }
-  function onFail(){
-    alert("false");
-  }
-  $scope.game = common.games[0];
+	function onPhotoDataSuccess() {
+		alert("ok");
+	}
+	function onFail() {
+		alert("false");
+	}
+	$scope.game = common.games[0];
 
-  $scope.echo = function(str) {
-    cordova.exec(function(res){alert(res.package_name)}, function(err) {
-      alert(err);
-    }, "Tools", "detect_package", [str]);
-  }
-  $scope.download = function(url) {
-    cordova.exec(function(res){
-      $scope.complete = res
-    }, function(err) {
-      alert(err);
-    }, "Downloader", "download", [url, {overwrite: true,fileName:"Sanguo_AndroidAuto_New_CGWPIP99.apk"}]);
-  }
+	$scope.echo = function(str) {
+		cordova.exec(function(res) {
+			alert(res.package_name)
+		},
+		function(err) {
+			alert(err);
+		},
+		"Tools", "detect_package", [str]);
+	}
+	$scope.download = function(url) {
+		cordova.exec(function(res) {
+			loaded(res.progress);
+		},
+		function(err) {
+			alert(err);
+		},
+		"Downloader", "download", [url, {
+			overwrite: true,
+			fileName: "Sanguo_AndroidAuto_New_CGWPIP99.apk"
+		}]);
+	}
 
-  $scope.data={total:0,completed:0};
-  $scope.total=0;
-  var i=0
-  var loaded = function(data){
-    $scope.data.completed = data;
-    $scope.$apply();
-  }
-//  setInterval(function(){
-//    loaded(i++);
-//  },1000)
-  $scope.download1 = function(url) {
-    var fileTransfer = new FileTransfer();
-    var uri = encodeURI(url);
-    var filePath = "/mnt/sdcard/download/test.apk";
-    fileTransfer.onprogress = function(progressEvent) {
-      if (progressEvent.lengthComputable) {
-        //$scope.complete = progressEvent.loaded;
-        //$scope.total = progressEvent.total;
-        loaded(progressEvent.loaded/progressEvent.total);
-        //loadingStatus.setPercentage(progressEvent.loaded / progressEvent.total);
-      } else {
-        loaded( i++ );
-        //loadingStatus.increment();
-      }
-    };
+	$scope.data = {
+		total: 0,
+		completed: 0
+	};
+	$scope.total = 0;
+	var i = 0
+	var loaded = function(data) {
+		$scope.data.completed = data;
+		$scope.complete = data;
+		$scope.$apply();
+	}
+	//  setInterval(function(){
+	//    loaded(i++);
+	//  },1000)
+	$scope.download1 = function(url) {
+		var fileTransfer = new FileTransfer();
+		var uri = encodeURI(url);
+		var filePath = "/mnt/sdcard/download/test.apk";
+		fileTransfer.onprogress = function(progressEvent) {
+			if (progressEvent.lengthComputable) {
+				//$scope.complete = progressEvent.loaded;
+				//$scope.total = progressEvent.total;
+				loaded(parseInt(progressEvent.loaded * 100 / progressEvent.total));
+				//loadingStatus.setPercentage(progressEvent.loaded / progressEvent.total);
+			} else {
+				loaded(i++);
+				//loadingStatus.increment();
+			}
+		};
 
-    fileTransfer.download(
-        uri,
-        filePath,
-        function(entry) {
-          alert("download complete: " + entry.fullPath);
-          alert($scope.complete);
-        },
-        function(error) {
-          alert("download error source " + error.source);
-          alert("download error target " + error.target);
-          alert("upload error code" + error.code);
-        },
-        false, { }
-        );
-  }
+		fileTransfer.download(
+		uri, filePath, function(entry) {
+			alert("download complete: " + entry.fullPath);
+			alert($scope.complete);
+		},
+		function(error) {
+			alert("download error source " + error.source);
+			alert("download error target " + error.target);
+			alert("upload error code" + error.code);
+		},
+		false, {});
+	}
 
-  $scope.fail = onFail;
+	$scope.fail = onFail;
 
-
-  $scope.capturePhoto = function(){
-    navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50, destinationType: destinationType.DATA_URL });
-  }
+	$scope.capturePhoto = function() {
+		navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
+			quality: 50,
+			destinationType: destinationType.DATA_URL
+		});
+	}
 }];
+
